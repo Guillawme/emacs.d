697ab83a00f94222fe8c4e5b79971556e63965d7
Guillaume Gaullier <guillaume.gaullier@gmail.com>
2016-10-14 10:28:39 -0600

    Simplified HTTPS configuration (it was not portable anyway)

--- config/https.el
+++ config/https.el
@@ -1,20 +1,17 @@
 ;; Emacs configuration
-;; Settings to enforce verification of HTTPS connections
-;; Taken from <https://glyph.twistedmatrix.com/2015/11/editor-malware.html>
+;; Settings to enforce verification of HTTPS connections.
+;; Taken from <https://glyph.twistedmatrix.com/2015/11/editor-malware.html>.
 
+;; Log important messages from HTTPS connections.
+(setq-default gnutls-log-level 1)
+
+;; Always check certificates validity.
 (setq-default tls-checktrust t)
+(setq-default gnutls-verify-error t)
 
-(let ((trustfile
-       ;; (replace-regexp-in-string
-       ;;  "\\\\" "/"
-       ;;  (replace-regexp-in-string
-       ;;   "\n" ""
-       ;;   (shell-command-to-string "python3 -m certifi")))
-       "/Library/Frameworks/Python.framework/Versions/3.5/lib/python3.5/site-packages/certifi/cacert.pem"))
-  (setq tls-program
-        (list
-         (format "gnutls-cli%s --x509cafile %s -p %%p %%h"
-                 (if (eq window-system 'w32) ".exe" "") trustfile)))
-  (setq gnutls-verify-error t)
-  (setq gnutls-trustfiles (list trustfile)))
+;; Path to a certificate bundle. This file should be maintened up to date.
+(setq-default gnutls-trustfiles "/Library/Frameworks/Python.framework/Versions/3.5/lib/python3.5/site-packages/certifi/cacert.pem")
+
+(setq-default tls-program
+              (format "gnutls-cli --x509cafile %s -p %%p %%h" gnutls-trustfiles))
 
