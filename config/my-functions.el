0e199bff3afcd639b282a1d8888042944bfe3d8c
Guillaume Gaullier <guillaume.gaullier@gmail.com>
2016-11-09 14:07:52 -0700

    Added missing docstrings in config/my-functions.el

--- config/my-functions.el
+++ config/my-functions.el
@@ -11,12 +11,12 @@
   (interactive)
   (insert (format-time-string "%X")))
 
-(defun my-split-window-vertically ()
+(defun my-split-window-vertically () "Split window vertically and display next buffer."
   (interactive)
   (split-window-vertically)
   (set-window-buffer (next-window) (other-buffer)))
 
-(defun my-split-window-horizontally ()
+(defun my-split-window-horizontally () "Split window horizontally and display next buffer."
   (interactive)
   (split-window-horizontally)
   (set-window-buffer (next-window) (other-buffer)))
