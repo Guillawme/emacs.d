dce0026a0093211fcafe2f71e6f2e4c2cd4efcdb
Guillaume Gaullier <guillaume@gaullier.org>
2018-02-16 15:32:39 -0700

    Get rid of things I never use, or don't use anymore

--- config/mac-os-x-parameters.el
+++ config/mac-os-x-parameters.el
@@ -19,25 +19,3 @@
      (define-key flyspell-mouse-map [down-mouse-3] #'flyspell-correct-word)
      (define-key flyspell-mouse-map [mouse-3] #'undefined)))
 
-;; Enable convenient shortcuts to type brackets
-;; with a French MacBook Pro keyboard
-
-;; Use bind-key utility
-(require 'bind-key)
-
-;; (bind-key "C-c n" '(insert-char "007e" 1 t)) ;; tilde
-;; (bind-key "C-(" '(insert-char "005b" 1 t)) ;; opening square bracket
-;; (bind-key "C-)" '(insert-char "005d" 1 t)) ;; closing square bracket
-;; (bind-key "M-(" '(insert-char "007b" 1 t)) ;; opening curly bracket
-;; (bind-key "M-)" '(insert-char "007d" 1 t)) ;; closing curly bracket
-;; (bind-key "C-:" '(insert-char "005c" 1 t)) ;; backslash
-;; (bind-key "C-!" '(insert-char "007c" 1 t)) ;; pipe
-
-(bind-key "C-c n" '(lambda () (interactive) (insert "~"))) ;; tilde
-(bind-key "C-(" '(lambda () (interactive) (insert "["))) ;; opening square bracket
-(bind-key "C-)" '(lambda () (interactive) (insert "]"))) ;; closing square bracket
-(bind-key "C-M-(" '(lambda () (interactive) (insert "{"))) ;; opening curly bracket
-(bind-key "C-M-)" '(lambda () (interactive) (insert "}"))) ;; closing curly bracket
-(bind-key "C-:" '(lambda () (interactive) (insert "\\"))) ;; backslash
-(bind-key "C-!" '(lambda () (interactive) (insert "|"))) ;; pipe
-
--- config/symbols.el
+++ config/symbols.el
@@ -4,9 +4,3 @@
 
 (bind-key "C-c =" "≃")
 
-;; All symbols below are already covered by the latin-1-prefix input method
-
-;(bind-key "C-c A" "Å")
-;(bind-key "C-c p" "±")
-;(bind-key "C-c i" "í")
-
