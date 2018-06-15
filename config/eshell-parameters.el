300261f4050a761d3f307634b1f366d89209aa4c
Guillaume Gaullier <guillaume@gaullier.org>
2018-06-14 18:07:05 -0600

    Use less calls to setq-default

--- config/eshell-parameters.el
+++ config/eshell-parameters.el
@@ -7,6 +7,6 @@
 (setq-default display-buffer-alist '(("\\`\\*e?shell" display-buffer-pop-up-window)))
 
 ;; Set eshell data directory and alias file.
-(setq-default eshell-directory-name "~/.emacs.d/eshell")
-(setq-default eshell-aliases-file "~/.emacs.d/eshell/aliases")
+(setq-default eshell-directory-name "~/.emacs.d/eshell"
+              eshell-aliases-file "~/.emacs.d/eshell/aliases")
 
