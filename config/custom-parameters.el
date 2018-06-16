ba30efdfe8c36d5afa163102db6b77d0161d8a9d
Guillaume Gaullier <guillaume@gaullier.org>
2018-06-16 15:18:30 -0600

    Now using ESS and polymode for R and Rmd files

--- config/custom-parameters.el
+++ config/custom-parameters.el
@@ -6,7 +6,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (edit-server magit which-key wc-goal-mode use-package pandoc-mode osx-trash org-plus-contrib markdown-mode ivy-hydra flyspell-correct-ivy exec-path-from-shell diminish counsel))))
+    (polymode ess edit-server magit which-key wc-goal-mode use-package pandoc-mode osx-trash org-plus-contrib markdown-mode ivy-hydra flyspell-correct-ivy exec-path-from-shell diminish counsel))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- config/prog-mode-parameters.el
+++ config/prog-mode-parameters.el
@@ -4,13 +4,22 @@
 
 ;; Number lines and highlight current line.
 (use-package display-line-numbers-mode
-  :hook prog-mode)
+  :hook (prog-mode poly-markdown-mode))
 (use-package hl-line-mode
-  :hook prog-mode)
+  :hook (prog-mode poly-markdown-mode))
 
 ;; Make scripts executable on save.
 (use-package prog-mode
   :config
   (add-hook 'after-save-hook
             'executable-make-buffer-file-executable-if-script-p))
+
+;; Use ESS for R, and polymode for Rmd files.
+(use-package ess
+  :ensure t
+  :mode "\\.R'")
+
+(use-package polymode
+  :ensure t
+  :mode ("\\.Rmd'" . poly-markdown+r-mode))
 
