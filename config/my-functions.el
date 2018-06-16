20ab57808bf3ce85599a22ab16b29d77068c2a53
Guillaume Gaullier <guillaume@gaullier.org>
2018-06-16 16:12:29 -0600

    Reformatted my-functions.el (more readable now)

--- config/my-functions.el
+++ config/my-functions.el
@@ -3,21 +3,25 @@
 ;; Some useful commands to be called by keybindings set in
 ;; ~/.emacs.d/config/keybindings.el
 
-(defun my-insert-date () "Insert current date at point."
+(defun my-insert-date ()
+  "Insert current date at point."
   (interactive)
   (insert (format-time-string "%x")))
 
-(defun my-insert-time () "Insert current time at point."
+(defun my-insert-time ()
+  "Insert current time at point."
   (interactive)
   (insert (format-time-string "%X")))
 
-(defun my-split-window-vertically () "Split window vertically, display next buffer and move point to new window."
+(defun my-split-window-vertically ()
+  "Split window vertically, display next buffer and move point to new window."
   (interactive)
   (split-window-vertically)
   (set-window-buffer (next-window) (other-buffer))
   (other-window 1))
 
-(defun my-split-window-horizontally () "Split window horizontally, display next buffer and move point to new window."
+(defun my-split-window-horizontally ()
+  "Split window horizontally, display next buffer and move point to new window."
   (interactive)
   (split-window-horizontally)
   (set-window-buffer (next-window) (other-buffer))
