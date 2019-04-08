a86c3318712bd3ff1ee1d61df3d6a893a4b8d51a
Guillaume Gaullier <guillaume@gaullier.org>
2019-04-08 14:43:05 -0600

    Rebuilt packages from scratch

--- config/custom-parameters.el
+++ config/custom-parameters.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (evil-mode org-plus-contrib magit gitlab-ci-mode flyspell-correct-ivy ivy-bibtex wc-goal-mode auctex pandoc-mode markdown-mode edit-server nov counsel-tramp ivy-hydra ivy-rich which-key osx-trash no-littering exec-path-from-shell diminish auto-package-update use-package))))
+    (evil org-journal org-plus-contrib ivy-rich ivy undo-tree which-key osx-trash no-littering exec-path-from-shell diminish auto-package-update use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
